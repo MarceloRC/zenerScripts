90debe9e2caeb6c54833d25d3a118cf72080ecad
andretapxure <49208838+andretapxure@users.noreply.github.com>
2019-04-27 12:23:04 -0300

    Create vmwaretools.sh

---
+++ vmwaretools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Esse script só foi testado em CentOS6 até o momento.
+#Nao esquecer de, no Host, clicar na VM, Botao Direito -> Guest -> Install VMWare Tools antes de executar
+
+mkdir /mnt/CD
+mount -tiso9660 /dev/cdrom /mnt/CD
+yum -y install gcc perl
+tar -zxvf /mnt/CD/VMwareTools-9.0.5-1065307.tar.gz
+cd vmware-tools-distrib/
+./vmware-install.pl -d
