6656d7355286188621174f48ef1a2df7a6056b7e
andretapxure <49208838+andretapxure@users.noreply.github.com>
2019-06-05 11:23:26 -0300

    Update zabbix_proxy_upgrade.sh

--- zabbix_proxy_upgrade.sh
+++ zabbix_proxy_upgrade.sh
@@ -14,6 +14,8 @@ esac
 yum -y clean all
 service zabbix-proxy stop && service zabbix-agent stop
 yum -y upgrade zabbix-proxy-sqlite3 zabbix-agent
+sed -i '/Server=/c\Server=zabbix.solidtecnologia.com.br' /etc/zabbix/zabbix_agentd.conf
+sed -i '/Server=/c\Server=zabbix.solidtecnologia.com.br' /etc/zabbix/zabbix_proxy.conf
 service zabbix-proxy start && service zabbix-agent start
 chkconfig zabbix-agent on && chkconfig zabbix-proxy on
 zabbix_agentd -V | grep zabbix_agentd
