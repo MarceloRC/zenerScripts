7107a77c3d7d652aef5b5af00ccde95bca4b3cca
andretapxure <49208838+andretapxure@users.noreply.github.com>
2019-06-05 09:17:44 -0300

    Update zabbix_proxy_upgrade.sh

--- zabbix_proxy_upgrade.sh
+++ zabbix_proxy_upgrade.sh
@@ -1,12 +1,12 @@
 case $(uname -a | cut -f6 -d '.') in
-"el6")
+6)
   rpm -Uvh https://repo.zabbix.com/zabbix/4.2/rhel/6/x86_64/zabbix-release-4.2-1.el6.noarch.rpm
   ;;
-"el7")
+7)
   rpm -Uvh https://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-release-4.2-1.el7.noarch.rpm
   ;;
 *)
-  echo "Versao nao compativel"
+  echo "*******************************Versao nao compativel**************************************"
   ;;
 esac  
 yum -y clean all
