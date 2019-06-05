a31dbd17efa6c5e567757b31cce80f70f5325fbd
andretapxure <49208838+andretapxure@users.noreply.github.com>
2019-06-05 09:03:57 -0300

    Create zabbix_proxy_upgrade.sh

---
+++ zabbix_proxy_upgrade.sh
@@ -0,0 +1,10 @@
+case $(uname -a | cut -f6 -d '.') in
+"el6")
+echo "rpm -Uvh https://repo.zabbix.com/zabbix/4.2/rhel/6/x86_64/zabbix-release-4.2-1.el6.noarch.rpm"
+"el7")
+echo "rpm -Uvh https://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-release-4.2-1.el7.noarch.rpm"
+*)
+echo "Versao nao compativel"
+esac  
+yum -y clean all
+yum upgrade zabbix-proxy-sqlite3 zabbix-agent
