e7209bc68e2a35ef849d495afc4a1b065839daec
Andre Tapxure <andretapxure@gmail.com>
2023-01-30 19:01:39 -0300

    Making log work

--- backupLogs.py
+++ backupLogs.py
@@ -12,8 +12,9 @@ db = mongoClient.logs
 
 
 def ProcessDir(customerName):
-    logging.info(f'{path}/{customerName}')
+    logging.info(f'Starting to process customer {customerName} on {path}')
     if customerName is None:
+        logging.warning(f'Customer {customerName} came empty, exiting')
         exit
     else:
         logDirs = []
@@ -34,23 +35,24 @@ def ProcessDir(customerName):
             ext = os.path.splitext(logFile)
             if (ext[1] == '.log'):
                 lines = open(f'{logDir.Path}/{logFile}', "r")
-                logging.info(f'openning file {logFile}')
+                logging.info(f'Openning file {logFile}')
                 for i, line in enumerate (lines):
                     lineNumber=str(i+1)
-                    logging.info(lineNumber)
+                    logging.debug(lineNumber)
                     try:
                         # 2023/01/29 12:25:23 INFO  : FILIPE/DEPARTAMENTO COMERCIAL/OUTROS/SARA REBELLO/Imagens/IMG_1315.JPG: Copied (new)
                         entries = line.split()
                         # Cutting out the line again to use the ':' as separator because space will not give us the file name correctly
                         entriesForFilename = line.split(':')
                     except:
-                        logging.WARNING(f'Cannot split line {lineNumber} of {logFile}')
+                        logging.warning(f'Cannot split line {lineNumber} of {logFile}')
                     logLine = ObjDict()
                     if len(entries) > 2:
                         logLine.Rotina = ext[0]
                         try:
                             logLine.Data = datetime.strptime(f'{entries[0]}T{entries[1]}Z0300' , '%Y/%m/%dT%H:%M:%SZ0300')
                         except:
+                            logging.warning(f'Cannot set date for line, setting default epoch')
                             logLine.Data = datetime.strptime(f'1969/12/31 21:00:00Z0300', '%Y/%m/%d %H:%M:%SZ0300')
                         logLine.Status = entries[2]
                     if len(entriesForFilename) > 4:
@@ -62,7 +64,7 @@ def ProcessDir(customerName):
                         logLine.Status = line
                     collection.insert_one(logLine)
 
-                    logging.info('Just appending the array')
+                    logging.debug(f'Just appending the array with {logLine.Rotina}')
 
 
 dir_list = os.listdir(path)
