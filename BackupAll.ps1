fe057f79b6c4dfaa17f88a4a6e0aff9350329a8c
MarceloRC <marcelorc2013@gmail.com>
2023-10-05 18:45:36 -0300

    update code backup rclone

---
+++ BackupAll.ps1
@@ -0,0 +1,1 @@
+c:\rclone\BackupOne.ps1 -SourceFolder E:\ -Transfers 150 -DestFolder drive-e -BucketName name-bucket -AccountName account-name
--- BackupOne.ps1
+++ BackupOne.ps1
@@ -1,16 +1,29 @@
 # This script is suposed to be fixed. Any extra arguments, put in ExtraArgs variable
 param (
 [String] $SourceFolder= 'd:\',
+[String] $SourceFolderLog= 'c:\rclone\logs\',
 [String] $Transfers = '80',
-[String] $AccountName='customer-fileserver',
+[String] $AccountName='customer',
+[String] $AccountNameLog='customer',
 [String] $BucketName='customer-fileserver',
+[String] $BucketNameLog='customer',
 [String] $DestFolder='drive-d',
-[String] $CustomerName='CustomerName',
+[String] $CustomerName='customer',
 [String] $ExtraArgs=""
 )
 # This vars shouldn't be changed
 $CurrentDate=Get-Date -Format "yyyy-MM-dd"
 
 
-New-Item -ItemType Directory -Force -Path "c:\rclone\$CurrentDate" > $null
-c:\rclone\rclone.exe sync --transfers $Transfers --filter-from 'c:\rclone\filters.txt' --log-level INFO  --log-file='c:\rclone\'$CurrentDate'\'$DestFolder'.log' $SourceFolder $AccountName':'$BucketName'/'$DestFolder
+New-Item -ItemType Directory -Force -Path "c:\rclone\logs\$CurrentDate" > $null
+
+# Sync Files in bucket
+c:\rclone\rclone.exe sync --transfers $Transfers --filter-from 'c:\rclone\filters.txt' --log-level INFO  --log-file='c:\rclone\logs\'$CurrentDate'\'$DestFolder'.log' "$SourceFolder" $AccountName':'$BucketName'/'"$DestFolder"
+
+# Sync Logs Server 
+c:\rclone\rclone.exe sync  --log-level INFO $SourceFolderLog $AccountNameLog':'$BucketNameLog/$CustomerName
+# Apagar logs antigos mais de 30 dias
+dir c:\rclone\logs | where { ((get-date)-$_.creationTime).days -gt 30 } | remove-item -force -Recurse
+
+# Enviar e-mail do log gerado do backup
+#c:\rclone\send-mail.ps1
