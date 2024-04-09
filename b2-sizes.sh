c0302b9ea149ecc78697b2d93a0eb0ac3699bf39
Marcelo Cardoso <46220302+MarceloRC@users.noreply.github.com>
2024-04-09 09:13:32 -0300

    Update b2-sizes.sh

--- b2-sizes.sh
+++ b2-sizes.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 now=$(date +'%Y%m%d')
 
-while read -r line; do
-    touch bucket_sizes_${now}.log
-    echo "authorizing ${line}"
-    b2 authorize-account $line 
-    b2 list-buckets > buckets_${now}.txt
-    while read -r linebucket; do
-        bucket=$(echo ${linebucket} | cut -f3 -d" ")
-        b2 get-bucket --showSize $bucket >> bucket_sizes_${now}.log
-    done < buckets_${now}.txt
+touch /home/ubuntu/zenerScripts/buckets_${now}.txt
+touch /home/ubuntu/zenerScripts/bucket_sizes_name_${now}.log
 
-done < accounts.txt
+while IFS=';' read -r v1 v2 v3; do
+    touch /home/ubuntu/zenerScripts/bucket_sizes_${now}.log
+    echo "authorizing ${v1} ${v2} ${v3} " 
+    b2 authorize-account $v1 $v2 
+    b2 list-buckets > /home/ubuntu/zenerScripts/buckets_${now}.txt
+    while read -r linebucket; do
+        bucket=$(echo ${linebucket} | cut -f3 -d" ") 
+        b2 get-bucket --showSize $bucket >> /home/ubuntu/zenerScripts/bucket_sizes_${now}.log
+    done < /home/ubuntu/zenerScripts/buckets_${now}.txt
+
+done < /home/ubuntu/zenerScripts/accounts.txt
+
+cat /home/ubuntu/zenerScripts/bucket_sizes_${now}.log | grep -e bucketName -e totalSize >> /home/ubuntu/zenerScripts/bucket_sizes_name_${now}.log
