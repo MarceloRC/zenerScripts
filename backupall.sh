d96552d47e24343fed13ebdaa77e6e63e7b13196
Marcelo Cardoso <46220302+MarceloRC@users.noreply.github.com>
2024-04-09 09:17:49 -0300

    Delete backupall.sh

--- backupall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#This script reads a simple text file (in this case, hosts) and gets the hosts info to call the pfbackup.sh script and make the backup of each firewall
-BASE="/home/ubuntu/zenerScripts/hosts"
-while IFS=, read -r customer_name desc host password username type
-do
-    echo "Backing up $customer_name"
-    /home/ubuntu/zenerScripts/pfbackup.sh $customer_name $desc $host $password $username $type
-done < $BASE
