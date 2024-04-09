9d773a53db0e6fc29b61210b65f9229af743f27a
marcelorc <marcelorc2013@gmail.com>
2024-04-09 09:15:33 -0300

    update

--- CheckAndSetup.ps1
+++ CheckAndSetup.ps1
@@ -7,15 +7,15 @@ if ($fail -eq 0){
     Write-Host ('Pre requisitos completados com sucesso. Iniciando instalacao') -ForegroundColor Green
     Write-Host ('Criando diretorio c:\rclone (Diretorio base de todo o conjunto de scripts)') -ForegroundColor Green
     New-Item -ItemType Directory -Force -Path 'c:\rclone\logs'> $null
-    New-Item -ItemType Directory -Force -Path 'c:\rclone\log-compactado'> $null   
+    #New-Item -ItemType Directory -Force -Path 'c:\rclone\log-compactado'> $null   
     #The following line is for Windows 2012 and bellow (TLS1.2)
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Write-Host ('Fazendo Download do arquivo BackupOne') -ForegroundColor Green
-    Invoke-WebRequest -URI https://github.com/andretapxure/zenerScripts/blob/master/BackupOne.ps1?raw=True -OutFile $pathRclone/BackupOne.ps1
+    Invoke-WebRequest -URI https://github.com/MarceloRC/zenerScripts/blob/master/BackupOne.ps1?raw=True -OutFile $pathRclone/BackupOne.ps1
     Write-Host ('Fazendo Download do arquivo BackupAll') -ForegroundColor Green
-    Invoke-WebRequest -URI https://github.com/andretapxure/zenerScripts/blob/master/BackupAll.ps1?raw=True -OutFile $pathRclone/BackupAll.ps1
+    Invoke-WebRequest -URI https://github.com/MarceloRC/zenerScripts/blob/master/BackupAll.ps1?raw=True -OutFile $pathRclone/BackupAll.ps1
     Write-Host ('Fazendo Download do arquivo Filters.txt') -ForegroundColor Green
-    Invoke-WebRequest -URI https://github.com/andretapxure/zenerScripts/blob/master/filters.txt?raw=True -OutFile $pathRclone/filters.txt
+    Invoke-WebRequest -URI https://github.com/MarceloRC/zenerScripts/blob/master/filters.txt?raw=True -OutFile $pathRclone/filters.txt
     Write-Host ('Fazendo Download do Rclone para a pasta temporaria do sistema %TEMP%') -ForegroundColor Green
     Invoke-WebRequest -URI https://downloads.rclone.org/rclone-current-windows-amd64.zip -OutFile $Env:TEMP/rclone.zip
     Write-Host ('Descompactando o arquivo rclone.exe do download que foi executado') -ForegroundColor Green
